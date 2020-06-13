383d26847f7f07aa04f576105ee6446f3859a766
Arun Shenoy <devarunshenoy99@gmail.com>
2020-06-13 14:11:02 +0530

    horses and humans

---
+++ horses-humans.py
@@ -0,0 +1,83 @@
+#https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%201%20-%20Part%208%20-%20Lesson%202%20-%20Notebook.ipynb#scrollTo=ClebU9NJg99G
+#https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%201%20-%20Part%208%20-%20Lesson%203%20-%20Notebook.ipynb#scrollTo=Fb1_lgobv81m
+
+import os
+import zipfile
+import tensorflow as tf
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+
+#Training data
+tmp = 'horse-or-human.zip'
+zip_ref = zipfile.ZipFile(tmp)
+zip_ref.extractall('horse-or-human')
+zip_ref.close()
+
+#Validation data
+local_zip = '/tmp/validation-horse-or-human.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/validation-horse-or-human')
+zip_ref.close()
+
+train_horse_dir = os.path.join('horse-or-human/horses')
+train_human_dir = os.path.join('horse-or-human/humans')
+validation_horse_dir = os.path.join('/tmp/validation-horse-or-human/horses')
+validation_human_dir = os.path.join('/tmp/validation-horse-or-human/humans')
+
+train_horse_names = os.listdir(train_horse_dir)
+print(train_horse_names[:10])
+train_human_names = os.listdir(train_human_dir)
+print(train_human_names[:10])
+
+print('Total training images for horses ', len(train_horse_names))
+print('Total training images for humans ', len(train_human_names))
+print('total validation horse images:', len(os.listdir(validation_horse_dir)))
+print('total validation human images:', len(os.listdir(validation_human_dir)))
+
+train_datagen = ImageDataGenerator(rescale=1/255)  # All images will be rescaled by 1./255
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+    'horse-or-human/',
+    target_size=(300, 300),             # This is the source directory for training images
+    batch_size=128,
+    class_mode='binary'                 # Since we use binary_crossentropy loss, we need binary labels
+)
+
+# Flow training images in batches of 128 using train_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+        '/tmp/validation-horse-or-human/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 300x300
+        batch_size=32,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape = (300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(optimizer=RMSprop(lr = 0.001), loss='binary_crossentropy', metrics = ['accuracy'])
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=8,      #We have almost 1024 images of batch size 128 hence 8 times to load all images per epoch
+    epochs=15,
+    verbose=1,               #Prints for every epoch, if 2 prints every 2 epochs
+    validation_data = validation_generator,
+    validation_steps = 8
+)
