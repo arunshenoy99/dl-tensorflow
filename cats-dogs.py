c93fbe8229576cc6ea03d57ee803f0a01182716c
Arun Shenoy <devarunshenoy99@gmail.com>
2020-06-16 14:23:17 +0530

    cats vs dogs kaggle challenge

---
+++ cats-dogs.py
@@ -0,0 +1,84 @@
+#wget --no-check-certificate \
+  #https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip \
+  #-O /tmp/cats_and_dogs_filtered.zip
+
+import os
+import zipfile
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+import PIL
+
+
+data_location = 'data/cats_and_dogs_filtered.zip'
+
+zip_ref = zipfile.ZipFile(data_location)
+zip_ref.extractall('data/')
+zip_ref.close()
+
+
+base_dir = 'data/cats_and_dogs_filtered'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cats_fnames = os.listdir(train_cats_dir)
+train_dogs_fnames = os.listdir(train_dogs_dir)
+validation_cats_fnames = os.listdir(validation_cats_dir)
+validation_dogs_fnames = os.listdir(validation_dogs_dir)
+
+print('Training')
+print('Cats: ', len(train_cats_fnames))
+print('Dogs: ', len(train_dogs_fnames))
+print('Validation')
+print('Cats: ', len(validation_cats_fnames))
+print('Dogs: ', len(validation_dogs_fnames))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape = (150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+
+model.compile(optimizer=RMSprop(lr = 0.001), loss = 'binary_crossentropy', metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+model.fit(
+    train_generator,
+    validation_data=validation_generator,
+    steps_per_epoch=100,
+    epochs = 15,
+    validation_steps = 50,
+    verbose = 2
+)
+
